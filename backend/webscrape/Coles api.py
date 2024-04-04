00575f416d9232030a834e83a139868019529687
Adam Lu <118642026+rysxtu@users.noreply.github.com>
2024-04-05 05:08:43 +1100

    Update to the api
    Price now reflects the cost when we purchase 1 carrot and so on. Does not determine cost based on grams and so on (makes our life simplier as we do not need to convert 1 carrot into grams)

--- backend/webscrape/Coles api.py
+++ backend/webscrape/Coles api.py
@@ -15,30 +15,38 @@ def scrap(url, pages):
             # temp1 helps to format the product
             product_name_temp1 = container.find('h2',class_="LinesEllipsis").text
             product_name = product_name_temp1[6:].split('|')[0].rstrip(" ")
-            try:
-                ppu = container.find("div", class_="price__calculation_method").text
-            except:
-                ppu = "$0"
-            # get rid of any unnecessary stuff in the string
-            ppu_split1 = ppu.split("|")
-            ppu_split2 = ppu_split1[0].split()
-            unit = ""
-            measurement = ""
-            for char in ppu_split2[-1]:
-                if char.isdigit():
-                    measurement += char
-                if not char.isdigit():
-                    unit += char
-            # ppsu stands for price per one standard unit. su stands for the standard unit, e.g 1 kg, 1 g, 1 ml
-            try:
-                if unit == 'L':
-                    prices.append({"product": product_name, "ppsu": f"{float(ppu_split2[0][1:])/1000:.4f}", "su": 'mL'})
-                elif unit == 'kg':
-                    prices.append({"product": product_name, "ppsu": f"{float(ppu_split2[0][1:])/1000:.4f}", "su": 'g'})
-                else:
-                    prices.append({"product": product_name, "ppsu": f"{float(ppu_split2[0][1:])/int(measurement):.4f}", "su": unit})
-            except:
-                prices.append({"product": product_name, "ppsu": 0, "su": unit})
+
+            if product_name_temp1.split('|')[1:]:
+                try:
+                    ppu = container.find("span", class_="price__value").text
+                except:
+                    ppu = "$0"
+                prices.append({"product": product_name, "ppsu": f"{float(ppu[1:]):.4f}", "su": product_name_temp1.split('|')[1][-5:].lstrip(" ")})
+            else:
+                try:
+                    ppu = container.find("div", class_="price__calculation_method").text
+                except:
+                    ppu = "$0"
+                # get rid of any unnecessary stuff in the string
+                ppu_split1 = ppu.split("|")
+                ppu_split2 = ppu_split1[0].split()
+                unit = ""
+                measurement = ""
+                for char in ppu_split2[-1]:
+                    if char.isdigit():
+                        measurement += char
+                    if not char.isdigit():
+                        unit += char
+                # ppsu stands for price per one standard unit. su stands for the standard unit, e.g 1 kg, 1 g, 1 ml
+                try:
+                    if unit == 'L':
+                        prices.append({"product": product_name, "ppsu": f"{float(ppu_split2[0][1:])/1000:.4f}", "su": 'mL'})
+                    elif unit == 'kg':
+                        prices.append({"product": product_name, "ppsu": f"{float(ppu_split2[0][1:])/1000:.4f}", "su": 'g'})
+                    else:
+                        prices.append({"product": product_name, "ppsu": f"{float(ppu_split2[0][1:])/int(measurement):.4f}", "su": unit})
+                except:
+                    prices.append({"product": product_name, "ppsu": 0, "su": unit})
 
 # print("Loading...")
 # coles prices of fruits and vegetables 

068a30921b655794b5ecf0288615674b39492fce
Adam Lu <118642026+rysxtu@users.noreply.github.com>
2024-04-05 07:09:08 +1100

    ~45 min into video
    There is an error when i import components into the app.jsx file, the whole web fails to load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,153 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Navbar />
+    <Hero />
+    <section class="py-4">
+      <div class="container-xl lg:container m-auto">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+          <div class="bg-gray-100 p-6 rounded-lg shadow-md">
+            <h2 class="text-2xl font-bold">For Developers</h2>
+            <p class="mt-2 mb-4">
+              Browse our React jobs and start your career today
+            </p>
+            <a
+              href="/jobs.html"
+              class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+            >
+              Browse Jobs
+            </a>
+          </div>
+          <div class="bg-indigo-100 p-6 rounded-lg shadow-md">
+            <h2 class="text-2xl font-bold">For Employers</h2>
+            <p class="mt-2 mb-4">
+              List your job to find the perfect developer for the role
+            </p>
+            <a
+              href="/add-job.html"
+              class="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+            >
+              Add Job
+            </a>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    </section>
+
+    <section class="bg-blue-50 px-4 py-10">
+      <div class="container-xl lg:container m-auto">
+        <h2 class="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          Browse Jobs
+        </h2>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+          <div class="bg-white rounded-xl shadow-md relative">
+            <div class="p-4">
+              <div class="mb-6">
+                <div class="text-gray-600 my-2">Full-Time</div>
+                <h3 class="text-xl font-bold">Senior React Developer</h3>
+              </div>
+
+              <div class="mb-5">
+               We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
+              </div>
+
+              <h3 class="text-indigo-500 mb-2">$70 - $80K / Year</h3>
+
+              <div class="border border-gray-100 mb-5"></div>
+
+              <div class="flex flex-col lg:flex-row justify-between mb-4">
+                <div class="text-orange-700 mb-3">
+                  <i class="fa-solid fa-location-dot text-lg"></i>
+                  Boston, MA
+                </div>
+                <a
+                  href="job.html"
+                  class="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                >
+                 Read More
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white rounded-xl shadow-md relative">
+            <div class="p-4">
+              <div class="mb-6">
+                <div class="text-gray-600 my-2">Remote</div>
+                <h3 class="text-xl font-bold">Front-End Engineer (React)</h3>
+              </div>
+
+              <div class="mb-5">
+               Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion...
+              </div>
+
+              <h3 class="text-indigo-500 mb-2">$70K - $80K / Year</h3>
+
+              <div class="border border-gray-100 mb-5"></div>
+
+              <div class="flex flex-col lg:flex-row justify-between mb-4">
+                <div class="text-orange-700 mb-3">
+                  <i class="fa-solid fa-location-dot text-lg"></i>
+                  Miami, FL
+                </div>
+                <a
+                  href="job.html"
+                  class="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                >
+                 Read More
+                </a>
+              </div>
+            </div>
+          </div>
+  
+          <div class="bg-white rounded-xl shadow-md relative">
+            <div class="p-4">
+              <div class="mb-6">
+                <div class="text-gray-600 my-2">Remote</div>
+                <h3 class="text-xl font-bold">React.js Developer</h3>
+              </div>
+
+              <div class="mb-5">
+                Are you passionate about front-end development? Join our team in vibrant Brooklyn, NY, and work on exciting projects that make a difference...
+              </div>
+
+              <h3 class="text-indigo-500 mb-2">$70K - $80K / Year</h3>
+
+              <div class="border border-gray-100 mb-5"></div>
+
+              <div class="flex flex-col lg:flex-row justify-between mb-4">
+                <div class="text-orange-700 mb-3">
+                  <i class="fa-solid fa-location-dot text-lg"></i>
+                  Brooklyn, NY
+                </div>
+                <a
+                  href="job.html"
+                  class="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                >
+                 Read More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </section>
+
+    <section class="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
     </>
   )
 }
 
 export default App
+

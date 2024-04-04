486c766c30dd291f664d443b9f5a3b9ce68b47bf
Adam Lu <118642026+rysxtu@users.noreply.github.com>
2024-04-05 07:42:26 +1100

    basic front end
    Same as from the tutorial. Will update it soon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,15 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-
+import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
+import HomeCards from './components/HomeCards.jsx'
 
 const App = () => {
   return (
     <>
     <Navbar />
-    <Hero />
-    <section class="py-4">
-      <div class="container-xl lg:container m-auto">
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <div class="bg-gray-100 p-6 rounded-lg shadow-md">
-            <h2 class="text-2xl font-bold">For Developers</h2>
-            <p class="mt-2 mb-4">
-              Browse our React jobs and start your career today
-            </p>
-            <a
-              href="/jobs.html"
-              class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
-            >
-              Browse Jobs
-            </a>
-          </div>
-          <div class="bg-indigo-100 p-6 rounded-lg shadow-md">
-            <h2 class="text-2xl font-bold">For Employers</h2>
-            <p class="mt-2 mb-4">
-              List your job to find the perfect developer for the role
-            </p>
-            <a
-              href="/add-job.html"
-              class="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
-            >
-              Add Job
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Hero title={"Hello"} subtitle={"bye"}/>
+    <HomeCards />
+    
+
 
     <section class="bg-blue-50 px-4 py-10">
       <div class="container-xl lg:container m-auto">
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/images/logo.png'
+import logo from '../assets/images/logo.png';
 import React from 'react'
 
 const Navbar = () => {

c5138a0f1e7fb47089b057157fdf41bbda89c9c8
Lawrence <78126433+TheLaw8@users.noreply.github.com>
2024-04-05 15:18:12 +1100

    Added icons and 404 error page

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,6 +1,19 @@
+import { Link } from 'react-router-dom';
+import { FaExclamationTriangle } from 'react-icons/fa';
+
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
-  )
-}
-export default NotFoundPage
+    <section className='text-center flex flex-col justify-center items-center h-96'>
+      <FaExclamationTriangle className='text-red-600 text-6xl mb-4' />
+      <h1 className='text-6xl font-bold mb-4'>404 Not Found</h1>
+      <p className='text-xl mb-5'>This page does not exist</p>
+      <Link
+        to='/'
+        className='text-white bg-green-600 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4'
+      >
+        Go Back
+      </Link>
+    </section>
+  );
+};
+export default NotFoundPage;

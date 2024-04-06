ec3170216cbff17bb4321b3d4df45418d9308486
OliverBao <83390446+OliverBao@users.noreply.github.com>
2024-04-06 12:09:51 +1100

    added cards for 2 meals 7 days

--- frontend/src/pages/MealPlanPage.jsx
+++ frontend/src/pages/MealPlanPage.jsx
@@ -1,10 +1,32 @@
 import NavBarCopy from "../components/NavbarCopy"
-
+import RecipeCard from "../components/RecipeCard"
 
 const MealPlanPage = () => {
   return (
     <>
         <NavBarCopy />
+        <br></br>
+
+        <div class="grid grid-cols-7 ap-4">
+          <div><RecipeCard name = "Beef Lasagna" /></div>
+          <div><RecipeCard name = "Chicken Lasagna"/></div>
+          <div><RecipeCard name = "Pork Lasagna"/></div>
+          <div><RecipeCard name = "Duck Lasagna"/></div>
+          <div><RecipeCard name = "Lamb Lasagna"/></div>
+          <div><RecipeCard name = "Quail Lasagna"/></div>
+          <div><RecipeCard name = "Venison Lasagna"/></div>
+        </div>
+
+        <div class="grid grid-cols-7 ap-4">
+          <div><RecipeCard name = "A Pasta"/></div>
+          <div><RecipeCard name = "B Pasta"/></div>
+          <div><RecipeCard name = "C Pasta"/></div>
+          <div><RecipeCard name = "D Pasta"/></div>
+          <div><RecipeCard name = "E Pasta"/></div>
+          <div><RecipeCard name = "F Pasta"/></div>
+          <div><RecipeCard name = "G Pasta"/></div>
+        </div>
+
     </>
   )
 }
